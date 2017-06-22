43d63614920e2f21ebaef9caacfcb2bfa73f16a6
scoomarts <soetaert.chris@gmail.com>
2017-06-22 13:54:17 +0200

    Mise en ligne du projet

--- app/config/prod.php
+++ app/config/prod.php
@@ -1,18 +1,16 @@
-
 <?php
 // Doctrine (db)
 $app['db.options'] = array(
     'driver'   => 'pdo_mysql',
     'charset'  => 'utf8',
-    'host'     => 'localhost',
+    'host'     => 'db687410862.db.1and1.com',
     'port' => '3306',
-    'dbname' => 'blog_forteroche',
-    'user' => 'root',
-    'password' => '',
+    'dbname' => 'db687410862',
+    'user' => 'dbo687410862',
+    'password' => 'chris24051985Ad',
 );
 
-
-$app['image_directory'] = 'http://forteroche/img';
+$app['image_directory'] = 'http://forteroche.scoomdev.eu/img';
 
 // define log parameters
 $app['monolog.level'] = 'WARNING';
--- src/Form/Type/CommentType.php
+++ src/Form/Type/CommentType.php
@@ -5,7 +5,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\hiddenType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class CommentType extends AbstractType {
 
